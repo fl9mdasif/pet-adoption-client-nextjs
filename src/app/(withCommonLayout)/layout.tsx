cbd3c2bdb770e3d7595428441d578f73f135f471
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2024-05-26 16:36:48 +0600

    initialize project and material ui

---
+++ src/app/(withCommonLayout)/layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/shared/Footer/Footer";
+import Navbar from "@/components/shared/Navbar/Navbar";
+import { Box } from "@mui/material";
+
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      <Box className="min-h-screen">{children}</Box>
+      <Footer />
+    </>
+  );
+};
+
+export default CommonLayout;
